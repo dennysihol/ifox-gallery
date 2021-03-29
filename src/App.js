bb815c8ae23cdbc1269aa5671af10102f50f31d3
Denny Sihol Ronaldo <57458599+dennysihol@users.noreply.github.com>
2021-03-29 18:45:12 +0700

    commit pukul 18.00

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import React from 'react'
+
+class App extends React.Component {
+  render () {
+    return React.createElement("h1", {}, "Hello Denny")
+  }
+}
+
+export default App;
