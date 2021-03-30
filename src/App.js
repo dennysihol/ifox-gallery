700d1e3bc578a42aae44fa112306769ec64a1889
Denny Sihol Ronaldo <57458599+dennysihol@users.noreply.github.com>
2021-03-30 17:51:42 +0700

    commit update custom hooks

--- src/App.js
+++ src/App.js
@@ -2,21 +2,29 @@ import './App.css';
 import React, { useEffect, useState } from 'react'
 import Navbar from './components/Navbar'
 import PhotoList from './components/PhotoList'
+import usePageBottom from './helpers/hooks/usePageBottom';
 
 
 
-export default function App () {
+export default function App (photo) {
 
   const [user, setUser] = useState('Denny Sihol Ronaldo')
   const [photos, setPhotos] = useState([])
+  const isPageBottom = usePageBottom()
   
   useEffect(() => {
-    fetch('https://picsum.photos/v2/list?page=10&limit=12')
-      .then(res => res.json())
-      .then(res => {
-        setPhotos(res)
-      });
-  }, [])
+    fetch('https://picsum.photos/v2/list')
+    .then(res => res.json())
+    .then(res => {
+      setPhotos(res)
+    });
+    if (!isPageBottom || !photos) return;
+    fetch('https://picsum.photos/v2/list')
+    .then(res => res.json())
+    .then(res => {
+      setPhotos(res)
+    });
+  }, [isPageBottom])
 
   return (
     <div>
@@ -28,7 +36,9 @@ export default function App () {
         <div className="row">
           {
             photos.map(photo => {
-              return  <PhotoList photo={photo} key={photo.id}></PhotoList>
+              return  (
+                <PhotoList photo={photo} key={photo.id}></PhotoList>
+              )
             })
           }
         </div>
--- src/components/PhotoList.jsx
+++ src/components/PhotoList.jsx
@@ -1,13 +1,13 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Swal from 'sweetalert2'
 
-export default function PhotoList () {
+export default function PhotoList ({photo}) {
     return (
         <div className="col-4">
-            <div className="card" style={{width: "18rem"}}>
-                <img src={this.props.photo.download_url} className="card-img-top" alt="Photo"></img>
-                <a href="#" className="btn btn-primary" onClick={ () => Swal.fire('Author :', this.props.photo.author)}>Show Author</a>
+            <div className="card" style={{margin: "10px"}}>
+                <img src={photo.download_url} className="card-img-top" alt="Photo"></img>
+                <a href="#" className="btn btn-primary" onClick={ () => Swal.fire('Author :', photo.author)}>Show Author</a>
             </div>
         </div>
     )
